f6c2a2e886f0b25ee400cb807aa0e75eb6b96c4e
Adi <adityasagarpython@gmail.com>
2024-06-21 10:38:17 -0300

    Notification UI
    Notification Stories and UI built.

--- src/stories/Notification.tsx
+++ src/stories/Notification.tsx
@@ -1,3 +1,7 @@
+import { MdCancel } from "react-icons/md";
+import "./../stories/public/index.css";
+import { RxCross2 } from "react-icons/rx";
+
 type NotificationProps = {
     notificationThumbnail: string,
     time: Date,
@@ -10,7 +14,22 @@ export default function Notification({notificationText, notificationThumbnail, t
   */
   return (
     <div className="notification-container">
-        
+      <div className="notification-content">
+        <div className="notification-thumbnail">
+          <img src={notificationThumbnail} alt="" />
+        </div>
+        <div className="notification-text">
+          <div className="notification-message">
+            {notificationText}
+          </div>
+          <div className="notification-time">
+            {time.getMilliseconds()}
+          </div>
+        </div>
+      <div className="notification-close">
+        <RxCross2 />
+      </div>
+      </div>
     </div>
   )
 }
