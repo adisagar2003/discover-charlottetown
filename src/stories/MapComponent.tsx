434cbf7ef1ff219aca80136f6e13e5e811b632a5
Adi <adityasagarpython@gmail.com>
2024-07-19 11:00:02 -0300

    Added deckgl layer to google maps

--- src/stories/MapComponent.tsx
+++ src/stories/MapComponent.tsx
@@ -1,27 +1,13 @@
-import { Map, Marker, useMap } from '@vis.gl/react-google-maps'
-import axios from 'axios';
-import { useEffect, useMemo, useState } from 'react'
-import api from '../utils/api';
-import { Location } from '../models/location.model.ts';
-import  DeckGL  from '@deck.gl/react';
-import { ScatterplotLayer } from '@deck.gl/layers';
-import {GoogleMapsOverlay as DeckOverlay} from '@deck.gl/google-maps';
-
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-use-before-define */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { Map, useMap } from '@vis.gl/react-google-maps'
+import { useEffect, useMemo } from 'react';
+import {GoogleMapsOverlay as DeckOverlay} from "@deck.gl/google-maps";
+import { ArcLayer, GeoJsonLayer } from 'deck.gl';
 
 export default function MapComponent() {
-  // fetch all the locations
-  const [locations, setLocation] = useState([]);
-  // define a memoized function for fetching the location list
-  const fetchLocationList = useMemo(() => async ()=> {
-    try {
-      const response = await axios.get(`${api}/api/location/10000`);
-      console.log(response);
-      setLocation(response.data.data);
-    }
-    catch (error) {
-      console.error('Error fetching product list: ', error);
-    }
-  }, []);
 
   function DeckGLOverlay(props) {
     const map = useMap();
@@ -29,27 +15,39 @@ export default function MapComponent() {
   
     useEffect(() => {
       overlay.setMap(map);
-      console.log({map});
       return () => overlay.setMap(null);
-    }, [map, overlay]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [map]);
   
     overlay.setProps(props);
     return null;
   }
-
-  useEffect(()=> {
-    fetchLocationList();
-  }, [fetchLocationList]);
-
+  const AIR_PORTS =
+  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
   const layers = [
-    new ScatterplotLayer({
-      id: '',
-      data: [
-        {position: [46.23465818393165,  -63.129953284633473 ]}
-      ],
-      getPosition: d => d.position,
-      getFillColor: [255, 0, 0, 100],
-      getRadius: 1000
+    new GeoJsonLayer({
+      id: 'airports',
+      data: AIR_PORTS,
+      // Styles
+      filled: true,
+      pointRadiusMinPixels: 2,
+      pointRadiusScale: 20,
+      getPointRadius: f => 11 - f.properties.scalerank,
+      getFillColor: [200, 0, 80, 200],
+      // Interactive props
+      pickable: true,
+      autoHighlight: true,
+    }),
+    new ArcLayer({
+      id: 'arcs',
+      data: AIR_PORTS,
+      dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
+      // Styles
+      getSourcePosition: () => [-0.4531566, 51.4709959], // London
+      getTargetPosition: f => f.geometry.coordinates,
+      getSourceColor: [0, 128, 200],
+      getTargetColor: [200, 0, 80],
+      getWidth: 1
     })
   ];
 
@@ -60,16 +58,9 @@ export default function MapComponent() {
               style={{width: '900px', height: '500px'}}
               defaultCenter={{lat: 46.23472677168374, lng: -63.134519289778176}}
               defaultZoom={15}
-              gestureHandling={'greedy'}
-              disableDefaultUI={false}
               >
-                <DeckGLOverlay layers={layers} />
-                {locations.map((location:Location)=> {
-                  return (<Marker title={location.title} onMouseOver={(e)=>console.log(e)}  position={{lat:location.latitude, lng:location.longitude}} />)
-                })}
-              </Map>
-          
-         
+                <DeckGLOverlay layers={layers}  />
+            </Map>
         </div>
     </div>
   )
