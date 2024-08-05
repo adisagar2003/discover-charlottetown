d10ee9cc4a960acb734fd9736722c298603b3305
Adi <adityasagarpython@gmail.com>
2024-08-05 14:32:22 -0300

    New Feat: Cloudinary uploads in register page

---
+++ src/stories/UploadWidget.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+import { useEffect, createContext, useState } from "react";
+
+// Create a context to manage the script loading state
+const CloudinaryScriptContext = createContext();
+
+const UploadWidget = ({uwConfig, setPublicId}) => {
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(()=>{
+        // Check if the script is already loaded
+        const uwScript = document.getElementById("uw");
+        if (!uwScript) {
+            // if not loaded, create one
+            const script = document.createElement("script");
+            script.setAttribute("async", "");
+            script.setAttribute("id", "uw");
+            script.src = "https://upload-widget.cloudinary.com/global/all.js";
+            script.addEventListener("load", () => setLoaded(true));
+            document.body.appendChild(script);
+            initializeCloudinaryWidget();
+        } else {
+            setLoaded(true);
+        }
+    }, [loaded]);
+
+    // create upload widget
+    const initializeCloudinaryWidget = () => {
+        if (loaded) {
+            console.log("creating upload widget");
+            const myWidget = window.cloudinary.createUploadWidget(
+                uwConfig,
+                (error, result) => {
+                    if (!error && result && result.event === "success") {
+                        console.log("Done! Here is the image info: ", result);
+                        setPublicId(result.info.public_id);
+                        document.getElementById("uploadedimage")
+                        .setAttribute("src", result.info.secure_url);
+                      }
+                      else {
+                        console.log(result);
+                      }
+                }
+            )
+    
+            document.getElementById("upload_widget").addEventListener("click",
+                function() {
+                    myWidget.open();
+                },
+                false
+            );
+        }
+    }
+    return (
+        <CloudinaryScriptContext.Provider value={{loaded}}>
+            <button
+                id="upload_widget"
+                className="cloudinary-button"
+                onClick={initializeCloudinaryWidget}
+            >
+                Upload
+            </button>
+        </CloudinaryScriptContext.Provider>
+    )
+}
+
+export default UploadWidget;
+export {CloudinaryScriptContext}
