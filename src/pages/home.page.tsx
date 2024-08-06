3f4a984a5dc00cbbf823f697225a532ba2d73e3f
Adi <adityasagarpython@gmail.com>
2024-08-06 14:39:49 -0300

    Fix: TS Error fix

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,12 +1,11 @@
 import MapComponent from '../stories/MapComponent'
-import Progress from '../stories/Progress.stories'
 
 export default function HomePage(){
   return (
         <div className='main'>
             <div className="map-content">
               <MapComponent />
-              
+
             </div>
           </div>
   )
