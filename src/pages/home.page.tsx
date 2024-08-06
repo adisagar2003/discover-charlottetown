73e07d06ceb105f64ede320446d70968b2431709
Adi <adityasagarpython@gmail.com>
2024-08-06 14:22:29 -0300

    New Feat: Progress ring created on bottom right

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,10 +1,12 @@
 import MapComponent from '../stories/MapComponent'
+import Progress from '../stories/Progress.stories'
 
 export default function HomePage(){
   return (
         <div className='main'>
             <div className="map-content">
               <MapComponent />
+              
             </div>
           </div>
   )
