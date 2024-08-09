853c613ebc452df414bdbe11189cefd6b84a304a
Adi <adityasagarpython@gmail.com>
2024-08-09 13:53:14 -0300

    LintFix: Making code look prettier

--- src/pages/browse.page.tsx
+++ src/pages/browse.page.tsx
@@ -1,12 +1,5 @@
-import { MdSearch } from "react-icons/md";
 import "./browse.page.css";
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-import { useEffect, useState } from "react";
-import axios from "axios";
-import api from "../utils/api";
-import LocationCard from "../stories/LocationCard";
-import { Location } from "../models/location.model";
+
 import Searchbar from "../stories/Searchbar";
 
 export default function BrowsePage() {
